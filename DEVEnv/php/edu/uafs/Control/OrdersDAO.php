3b9164b3aad6b88e8d37756568314fec45e88b42
mgarci14@g.uafs.edu <mgarci14@g.uafs.edu>
2023-10-30 13:14:43 -0500

    testing

---
+++ DEVEnv/php/edu/uafs/Control/OrdersDAO.php
@@ -0,0 +1,201 @@
+<?php
+
+require('/srv/www/htdocs/php/edu/uafs/Model/Orders.php');
+
+class OrdersDAO {
+    private $user="root";
+    private $password ="Garmon22";
+  
+
+   
+
+    public function getAllItemFromDatabase() {
+        $myConsumersOrds = array();
+        $sql = "SELECT * FROM orders;";
+
+        $con = null;
+        $pstmt = null;
+        
+        
+        try {
+            
+
+            ini_set('display_errors', 1);
+            ini_set('display_startup_errors', 1);
+            error_reporting(E_ALL); 
+            
+            $con = new mysqli("localhost", $this->user, $this->password, "Testing");
+            
+                if ($con->connect_error) {
+                    die("Connection failed: " . $con->connect_error);
+                    
+                }
+                $pstmt = $con->prepare($sql);
+               
+                $pstmt->execute();
+                $result = $pstmt->get_result();
+
+                if ($result != null) {
+                    $i = 0;
+                    while ($row = $result->fetch_assoc()) {
+                        $csOrd = new Orders();
+                        $csOrd->setOrderID ($row["orderID"]);
+                        $csOrd->setCustID($row["custID"]);
+                        $csOrd->setIsCustomOrder($row["isCustomOrder"]);
+                        $csOrd->setAmount($row['Amount']);
+                        $csOrd->setDate($row['Date']);
+                        $csOrd->setTransID($row['TransID']);
+                        $csOrd->setStatus($row['Status']);                      
+                        $myConsumersOrds[$i] = $csOrd;
+                        $i++; 
+                    }
+                }
+
+                $con->close();
+                return $myConsumersOrds;
+            
+        } catch (Exception $e) {
+            echo "Database Connection Failed In the getAllItemDatabase from Consumer class!";
+            echo $e->getMessage();
+            return null;
+        }
+    }
+
+    public function insertOrder($custID,$isCustomOrder,$amount,$orderDate,$transID,$orderStatus){
+        $con = null;
+        $pstmt = null;
+        $sql = "INSERT INTO orders(CustID,IsCustomOrder,Amount,OrderDate,TransID,OrderStatus)VALUES(?,?,?,?,?,?)";
+
+        try{
+            //establish connection
+            $con = new mysqli("localhost",$this->user,$this->password,"Testing");
+
+            //prepare statement
+
+            $pstmt = $con->prepare($sql);
+            $pstmt->bind_param("isdsis", $custID,$isCustomOrder, $amount, $orderDate,$transID,$orderStatus);
+
+            //execute query
+            $pstmt->execute();
+
+            //return result if needed
+            //close connection
+            $con->close();
+
+            return true; 
+
+        }catch(Exception $e) {
+            echo "Database Connection Failed In the getAllItemDatabase from Consumer class!";
+            echo $e->getMessage();
+            return false;
+        }
+    }
+
+    public function updateOrder($orderID,$custID,$isCustomOrder,$amount,$orderDate,$transID,$orderStatus){
+
+        $con = null;
+        $pstmt = null;
+        $sql = "UPDATE orders SET CustID=?,IsCustomOrder=?,Amount=?,OrderDate=?,TransID=?,OrderStatus=? WHERE(OrderID=?)";
+
+        try{    
+            //establish a connection
+            $con = new mysqli("localhost", $this->user,$this->password,"Testing");
+
+            //prepare statement
+            $pstmt = $con->prepare($sql);
+            $pstmt->bind_param("isdsisi",$custID,$isCustomOrder, $amount, $orderDate,$transID,$orderStatus,$orderID);
+            
+            //execute query
+            $pstmt->execute();
+            
+            //close connection
+            $con->close();
+        }catch(Exception $e) {
+            echo "Database Connection Failed In the getAllItemDatabase from Consumer class!";
+            echo $e->getMessage();
+            return false;
+        }
+    }
+
+    public function deleteOrder( $orderID ){
+        $con = null;
+        $pstmt = null;
+        $sql = "DELETE FROM orders WHERE OrderID=?";
+
+        try{
+            //establish connection
+            $con = new mysqli("localhost",$this->user,$this->password,"Testing");
+
+            //prepare statement
+            $pstmt = $con->prepare($sql);
+            $pstmt->bind_param("i",$orderID);
+
+            //execute
+            $pstmt->execute();
+
+            $con->close();
+
+            return true; 
+        }catch(Exception $e) {
+            echo "Database Connection Failed In the getAllItemDatabase from Consumer class!";
+            echo $e->getMessage();
+            return false;
+            
+        }
+
+
+    }
+
+    public function listOrderBasedOnCustID( $custID ){
+
+        $con = null;
+        $pstmt = null;
+        $sql = "SELECT * FROM orders WHERE CustID=?;";
+        $orders = array(); 
+        try{
+            //establish connection
+            $con = new mysqli("localhost", $this->user,$this->password,"Testing");
+            //prepare statement
+            $pstmt = $con->prepare($sql);
+            $pstmt->bind_param("i",$custID);
+
+            //execute
+            $pstmt->execute();
+
+            //return resultset
+
+            $result = $pstmt->get_result();
+
+            if($result!=null){
+                $i=0;
+                while ($row = $result->fetch_assoc()) {
+                    $csOrd = new Orders();
+                    $csOrd->setOrderID ($row["orderID"]);
+                    $csOrd->setCustID($row["custID"]);
+                    $csOrd->setIsCustomOrder($row["isCustomOrder"]);
+                    $csOrd->setAmount($row['Amount']);
+                    $csOrd->setDate($row['Date']);
+                    $csOrd->setTransID($row['TransID']);
+                    $csOrd->setStatus($row['Status']);                      
+                    $orders[$i] = $csOrd;
+                    $i++; 
+                }
+
+            }
+
+            //close connection 
+
+            $con->close();
+
+            return $orders;
+
+        }catch(Exception $e) {
+            echo "Database Connection Failed In the getAllItemDatabase from Consumer class!";
+            echo $e->getMessage();
+            return null;
+        }
+
+    }
+
+}
+?>
---
+++ DEVEnv/webApp/orders.php
@@ -0,0 +1,183 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+   
+</head>
+    <title>orders</title>
+</head>
+<body>
+
+    <header>
+        
+        <nav class="navbar" data-bs-theme="dark">
+                
+                <h2 style="color: white; font-family: anton;"><a id="logoLink" href="/">TB Borders</a></h2>
+                <form class="form-inline my-2 my-lg-0" style="margin-left: 5%;">
+                    <div class="container ">
+                        <div class="row">
+                                <div class="col-9">
+                                    <input id="searchBar" class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                                </div>
+                                <div class="col" >
+                                        <button id="searchBtn" class="btn btn-primary" type="submit">
+                                            <i class="bi bi-search"></i>
+                                        </button>
+                                </div>
+                            
+                        </div>
+                    </div>
+                </form>
+                <ul class="nav" >
+                    
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#" style="color: white;">
+                            <i class="bi bi-card-list"></i>
+                        </a>
+
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">
+                            <i class="bi bi-cart-check"></i>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">
+                            <i class="bi bi-door-open"></i>
+                        </a>
+                    </li>
+                   
+                </ul>
+          </nav>
+       
+    </header>
+
+    <main>
+
+    <?php
+   
+    ?>
+     
+        <div class="container-lg">
+            <table class="table table-hover">
+                <thead>
+                        <tr>
+                            <th scope="col">ID</th>
+                            <th scope="col">Date</th>
+                            <th scope="col">Status</th>
+                            <th scope="col">Amount</th>
+                            <th scope="col">Description</th>
+                            <th scope="col"></th>
+                        </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <th scope="row">1</th>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                    <td>Always specify both the height <br>and width attributes for images</td>
+                    <td><img src="images/wf1.jpeg" alt="pic" width="80" height="80"></td>
+                </tr>
+                <tr>
+                    <th scope="row">2</th>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                    <td>@fat</td>
+                    <td>Always specify both the height <br>and width attributes for images</td>
+                    <td><img src="images/wf2.jpeg" alt="pic" width="80" height="80"></td>
+                </tr>
+                <tr>
+                    <th scope="row">2</th>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                    <td>@fat</td>
+                    <td>Always specify both the height <br>and width attributes for images</td>
+                    <td><img src="images/wf2.jpeg" alt="pic" width="80" height="80"></td>
+                </tr>
+                <tr>
+                    <th scope="row">2</th>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                    <td>@fat</td>
+                    <td>Always specify both the height <br>and width attributes for images</td>
+                    <td><img src="images/wf1.jpeg" alt="pic" width="80" height="80"></td>
+                </tr>
+                <tr>
+                    <th scope="row">2</th>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                    <td>@fat</td>
+                    <td>Always specify both the height <br>and width attributes for images</td>
+                    <td><img src="images/wf3.jpeg" alt="pic" width="80" height="80"></td>
+                </tr>
+                <tr>
+                    <th scope="row">2</th>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                    <td>@fat</td>
+                    <td>Always specify both the height <br>and width attributes for images</td>
+                    <td><img src="images/wf3.jpeg" alt="pic" width="80" height="80"></td>
+                </tr>
+                <tr>
+                    <th scope="row">2</th>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                    <td>@fat</td>
+                    <td>Always specify both the height <br>and width attributes for images</td>
+                    <td><img src="images/wf1.jpeg" alt="pic" width="80" height="80"></td>
+                </tr>
+            
+                </tbody>
+            </table>
+            </div>
+    </main>
+
+   
+<!-- Footer -->
+<footer class="bg-dark text-center text-white " >
+                <!-- Grid container -->
+                <div class="container p-1" >
+                    <!-- Section: Social media -->
+                    <section class="mb-1" >
+
+                    <!-- Twitter -->
+                    <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+                        ><i class="bi bi-twitter"></i>
+                    </a>
+
+                    <!-- facebook -->
+                    <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+                        ><i class="bi bi-facebook"></i></a>
+
+                    <!-- Instagram -->
+                    <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+                        ><i class="bi bi-instagram"></i></a>
+
+                    <!-- Linkedin -->
+                    <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+                        ><i class="bi bi-linkedin"></i></a>
+
+                    <!-- Github -->
+                    <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+                        ><i class="bi bi-github"></i></a>
+                    </section>
+
+                <!-- Copyright -->
+                <div class="text-center " style="background-color: rgba(0, 0, 0, 0.2);">
+                    © 2020 Copyright:
+                    <a class="text-white" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+                </div>
+                <!-- Copyright -->
+</footer>
+<!-- Footer -->
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+
+</html>
