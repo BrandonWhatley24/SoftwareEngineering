08cd7eaf0794e25c69d06c9bf885d953d8712d94
Brandon Whatley <bwhatl00@g.uafs.edu>
2023-10-30 17:47:38 -0500

    Added basic js functions

---
+++ Frontend App/FrontEnd Homepage/HomeFrontFunctions.js
@@ -0,0 +1,11 @@
+function filterSelection(filterType){
+    console.log("FilterSelection called with type - " + filterType);
+}
+
+function initalPageLoad(){
+    console.log("initalPageLoad called")
+}
+
+function addItemToCart(item){
+    console.log("addItemToCart called with item being" + item);
+}
